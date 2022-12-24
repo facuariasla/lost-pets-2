f0349b9c33c2f37fce80b1b9968db720e9f0ef31
Facundo Arias <facundolautaroarias@gmail.com>
2022-12-24 14:26:37 -0300

    assets added - icon added in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,25 @@
-import { Stack, Box, Icon } from "@chakra-ui/react";
+import { Stack, Box, Icon, Image } from "@chakra-ui/react";
 import { FaPaw } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import MenuBurger from "./MenuBurger";
-
+import pawerblackicon from "../../assets/pawerlogo_black.svg";
 MenuBurger;
 
 const HeaderOne = () => {
   return (
-    <Box as="header" bgColor="pinkie.500" >
+    <Box as="header" bgColor="pinkie.500">
       <Stack
         h="10vh"
         bgColor="pinkie.500"
         direction="row"
         alignItems="center"
         justifyContent="space-between"
-        paddingX={[4,4,10]}
+        paddingX={[4, 4, 10]}
       >
         <Stack>
           <Link to="/">
-            <Icon as={FaPaw} h={10} w={10} cursor="pointer" />
+            {/* <Icon as={FaPaw} h={10} w={10} cursor="pointer" /> */}
+            <Image boxSize="60px" src={pawerblackicon} objectFit="cover" />
           </Link>
         </Stack>
 
@@ -27,7 +28,7 @@ const HeaderOne = () => {
           <MenuBurger />
         </Box>
       </Stack>
-      </Box>
+    </Box>
   );
 };
 
